8e9c155ac5ec29b15808505015fd16ab15ad91ed
Petro Cardoso <petrolcds@gmail.com>
2024-12-30 21:52:54 -0300

    Ajustes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   apps: [
     {
-      detached: true,
       max_restarts: 5,
       name: "server",
       script: "./dist/server.js",
@@ -9,12 +8,7 @@ module.exports = {
       // error_file: "/var/log/pm2/server_error.log",
       // out_file: "/var/log/pm2/server_out.log",
       exec_mode: "cluster",
-      instances: 2,
       // combine_logs: true,
-      env: {
-        NODE_ENV: "production",
-        NODE_CLUSTER_SCHED_POLICY: "rr",
-      },
     },
   ],
 };
